9823427beedc84a0a4b83cec6de22632f8ff275b
Jin Hyeok Oh <ojinhyug@gmail.com>
2024-11-06 15:16:40 +0900

    week5 complete

--- week5/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week5.git
-
-go 1.23.1
-
-require golang.org/x/sync v0.8.0
--- week5/lab2/main.go
+++ week5/lab2/main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
+	//"net/http"
 	"os"
-	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/blob/main/week5/lab2/config"
+	//"os/signal"
+	//"syscall"
+	//"time"
 
-	"golang.org/x/sync/errgroup"
+	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/blob/main/week5/lab2/config"
+	//"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -20,6 +23,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	/*ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil{
 		return err
@@ -32,6 +37,7 @@ func run(ctx context.Context) error {
 	log.Printf("start with: %v", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
@@ -50,4 +56,19 @@ func run(ctx context.Context) error {
 		log.Printf("failed to shutdown: %+v", err)
 	}
 	return eg.Wait()
+	*/
+
+	cfg, err := config.New()
+	if err != nil{
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil{
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
