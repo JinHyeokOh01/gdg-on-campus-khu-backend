9ec98183e5f5976a605d01590fb6fa685a0d1809
Jin Hyeok Oh <ojinhyug@gmail.com>
2024-11-06 23:04:23 +0900

    update bst

--- week2/tree/bst.go
+++ week2/tree/bst.go
@@ -1,31 +1,39 @@
 package bst
 
-import(
+import (
 	"fmt"
 )
 
-type Node struct{
+type Node struct {
 	Value int
-	Left *Node
+	Left  *Node
 	Right *Node
 }
 
-func MakeNode(num int) *Node{
+func MakeNode(num int) *Node {
 	return &Node{Value: num}
 }
 
-func (tree *Node) Insert(num int){
-	if(tree == nil){
-		MakeNode(num)
-	}else if tree.Value > num {
-		tree.Right = MakeNode(num)
-	}else{
-		tree.Left = MakeNode(num)
+func (tree *Node) Insert(num int) {
+	if tree == nil {
+		tree = MakeNode(num)
+	} else if num < tree.Value {
+		if tree.Left == nil {
+			tree.Left = MakeNode(num) 
+		} else {
+			tree.Left.Insert(num)
+		}
+	} else {
+		if tree.Right == nil {
+			tree.Right = MakeNode(num)
+		} else {
+			tree.Right.Insert(num)
+		}
 	}
 }
 
-func InOrder(n *Node){
-	if(n != nil){
+func InOrder(n *Node) {
+	if n != nil {
 		InOrder(n.Left)
 		fmt.Print(n.Value, " ")
 		InOrder(n.Right)
--- week2/tree/go.mod
+++ week2/tree/go.mod
@@ -1,3 +1,3 @@
-module github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week2/tree
+module JinHyeokOh01/gdg-on-campus-khu-backend/blob/main/week2/tree
 
-go 1.23.2
+go 1.23.1
