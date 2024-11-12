410aa9d4009b2f8b024479b8cdc969ad29cbba0b
Jin Hyeok Oh <ojinhyug@gmail.com>
2024-11-12 20:50:47 +0900

    week6 upload

--- week6/chapter17/testutil/handler.go
+++ week6/chapter17/testutil/handler.go
@@ -17,7 +17,7 @@ func AssertJSON(t *testing.T, want, got []byte){
 		t.Fatalf("cannot unmarshal want %q: %v", want, err)
 	}
 	if err := json.Unmarshal(got, &jg); err != nil{
-		t.Fatalf("cannot unmarshal want %q: %v", want, err)
+		t.Fatalf("cannot unmarshal got %q: %v", got, err)
 	}
 	if diff := cmp.Diff(jg, jw); diff != ""{
 		t.Errorf("got differs: (-got +want)\n%s", diff)
