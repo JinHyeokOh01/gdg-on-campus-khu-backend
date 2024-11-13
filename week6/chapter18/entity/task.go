7010667f37345316a49d49d4271c343ac12fc0df
Jin Hyeok Oh <ojinhyug@gmail.com>
2024-11-13 15:40:29 +0900

    chapter 18 complete

--- week6/chapter18/entity/task.go
+++ week6/chapter18/entity/task.go
@@ -15,7 +15,8 @@ type Task struct{
 	ID	TaskID	`json:"id"`
 	Title string	`json:"title"`
 	Status TaskStatus	`json:"status"`
-	Created time.Time	`json:"created"` 
+	Created time.Time	`json:"created"`
+	Modified time.Time	`json:"modified" db:"modified"`
 }
 
 type Tasks []*Task
