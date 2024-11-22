5b51f193382a846db918fce857064154156fe866
Jin Hyeok Oh <ojinhyug@gmail.com>
2024-11-22 16:00:24 +0900

    feat: week8 initialize

---
+++ week8/mux.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week8/clock"
+	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week8/config"
+	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week8/handler"
+	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week8/service"
+	"github.com/JinHyeokOh01/gdg-on-campus-khu-backend/week8/store"
+)
+
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error){
+	mux := chi.NewRouter()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json; charset=stf-8")
+		_, _ = w.Write([]byte(`{"status":"ok"}`))
+	})
+	v := validator.New()
+	db, cleanup, err := store.New(ctx, cfg) // connection 생성
+	if err != nil{
+		return nil, cleanup, err
+	}
+	r := store.Repository{Clocker: clock.RealClocker{}}
+	at := &handler.AddTask{
+		Service: &service.AddTask{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{
+		Service: &service.ListTask{DB: db, Repo: &r},
+	}
+	mux.Get("/tasks", lt.ServeHTTP)
+	ru := &handler.RegisterUser{
+		Service: &service.RegisterUser{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/register", ru.ServeHTTP)
+	return mux, cleanup, nil
+}
