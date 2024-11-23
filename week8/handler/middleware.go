291f0042b31d56e1cf96ba1de257ec7739f8ee7d
Jin Hyeok Oh <ojinhyug@gmail.com>
2024-11-24 03:08:30 +0900

    feat: section 86 complete

---
+++ week8/handler/middleware.go
@@ -0,0 +1,34 @@
+package handler
+
+import(
+	"fmt"
+)
+
+func AuthMiddleware(j *auth.JWTer) func(next http.Handler) http.Handler{
+	return func(next http.Handler) http.Handler{
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			req, err := j.FillContext(r)
+			if err != nil{
+				RespondJSON(r.Context(), w, ErrResponse{
+					Message: "not find auth info",
+					Details: []string{err,Error()},
+				}, http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func AdminMiddleware(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		log.Print("AdminMiddleware")
+		if !auth.IsAdmin(r.Context()){
+			RespondJSON(r.Context(), w, ErrResponse{
+				Message: "not admin",
+			}, http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
